206362061e4ef0a917de9fad561aac97938ef835
L344S <7474@holbertonstudents.com>
2024-01-04 00:47:43 +0100

    Added exexecute function that handle argument

---
+++ execute.c
@@ -0,0 +1,66 @@
+#include "header.h"
+
+/**
+* execute - Entry point of the program
+* @file: Argv[1] (file to be read passed as argument)
+*
+* --------------- Description ---------------
+* Handle the argument <filename> passed to the program
+* Read the file and execute the instructions inside it
+*
+* Return: Always 0 if sucess or EXIT_FAILURE
+*/
+
+void execute(char *file)
+{
+	FILE *file_that_will_be_read; /* pointer to the file that will be read */
+	char *line = NULL; /* store the line read from the file */
+
+	size_t BUFSIZE = 0; /* size of the buffer */
+	ssize_t char_read; /* store the number of characters read return by getline */
+	int line_number = 1; /* store the line number to keep track of process */
+
+	int exectution_status = 0; /* store the status of the execution */
+
+	my_stack_t *stack = NULL; /* pointer to the head of the stack */
+
+	file_that_will_be_read = fopen(file, "r"); /* open the file in read mode */
+	if (file_that_will_be_read == NULL) /* Error : If the file can't be open */
+	{
+		/* print the error message in STDERR saying the file can't be open */
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file);
+		exit(EXIT_FAILURE); /* quit the program on failure */
+	}
+
+	/* if the file can be open then we can continue */
+	/* loop through the file line by line until EOF */
+	while ((char_read = getline(&line, &BUFSIZE, file_that_will_be_read)) != -1)
+	{
+        /* if the line is not empty and not a comment */
+        if (line[0] != '\n' && line[0] != '#')
+        {
+            /* execute the instruction */
+            exectution_status = execute_instruction(line, &stack, line_number);
+            /* if the execution fail */
+            if (exectution_status == EXIT_FAILURE)
+            {
+                /* free the memory allocated for the line */
+                free(line);
+                /* free the memory allocated for the stack */
+                /*frees(&stack);*/
+                /* close the file */
+                fclose(file_that_will_be_read);
+                /* quit the program on failure */
+                exit(EXIT_FAILURE);
+            }
+        }
+        line_number++; /* increment the line number */
+	}
+
+	free(line);
+    /*frees(&stack);*/
+	fclose(file_that_will_be_read);
+}
+
+
+
