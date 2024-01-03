facd9819c04dd018755aeddab3d2fac922ce6567
L344S <7474@holbertonstudents.com>
2024-01-04 00:51:03 +0100

    Added helper.c that contains frees function (free the stack)

---
+++ helpers.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+/**
+* frees - Entry point of the program
+* @stack: Pointer to the head of the stack
+* --------------- Description ---------------
+* Free the memory allocated for the stack
+* Return: Always 0 if sucess or EXIT_FAILURE
+*/
+void frees(my_stack_t **stack)
+{
+	my_stack_t *current_node = *stack;
+
+	while (current_node != NULL)
+	{
+		*stack = (*stack)->next;
+		free(current_node);
+		current_node = *stack;
+	}
+}
