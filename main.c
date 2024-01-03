60430b98a3117da3127ea12c9bb052e731b2b483
L344S <7474@holbertonstudents.com>
2024-01-03 09:37:53 +0100

    Created main.c file that handle error
    If the user does not give any file or more than one argument to your program, print the error message

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "header.h"
+
+/**
+* main - Entry point of the program
+* @argc: Argument count (number of arguments)
+* @argv: Argument vector (array of strings passed as arguments)
+*
+* --------------- Description ---------------
+* If the user does not give any file or more than one argument
+* to your program print the error message
+*
+* Return: Always 0 if sucess or EXIT_FAILURE
+*/
+
+int main(int argc, char **argv)
+{
+	/* Error : If there are more than 2 arguments */
+	if (argc != 2)
+	{
+		/* Print the error message in STDERR */
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE); /* quit the program on failure */
+	}
+
+	return (0); /* return 0 if sucess so the program can continue */
+}
