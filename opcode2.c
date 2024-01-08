60f0ae8f4b64a2b5b249a637af9c341ad528e011
L344S <7474@holbertonstudents.com>
2024-01-08 10:01:56 +0100

    Added comment for better clarity

--- opcode2.c
+++ opcode2.c
@@ -10,17 +10,21 @@
 */
 void add(my_stack_t **stack, unsigned int line_number)
 {
+	/* Create a temporary pointer */
 	my_stack_t *temp;
-
+	/* Check if the stack is empty or has only one element */
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	/* add the value to the second element of the top of the stack */
 	(*stack)->next->n += (*stack)->n;
+	/* store the address of the top of the stack in the temporary pointer */
 	temp = *stack;
+	/* update the stack pointer to the next node */
 	*stack = (*stack)->next;
+	/* free the memory allocated to the top of the stack */
 	free(temp);
 }
 
@@ -34,6 +38,8 @@ void add(my_stack_t **stack, unsigned int line_number)
 */
 void nop(my_stack_t **stack, unsigned int line_number)
 {
+	/* unused parameters */
 	(void)stack;
 	(void)line_number;
+	/* do nothing */
 }
